7463e20dd267ab461b4de5c60ce59c65f13135f0
Kris <kris.wolff@utah.edu>
2018-09-08 16:36:52 -0600

    Added enums for employment and tax status (single, married, etc)
    Need for user inputs to solve different tax rates given.

---
+++ src/TaxDogg/Employment.java
@@ -0,0 +1,14 @@
+package TaxDogg;
+
+/**
+ * List of accepted tax form employment that user would be 
+ * able to select to find correct tax rates.
+ * 
+ * @author Last Place
+ */
+public enum Employment {
+	
+	EMPLOYED,
+	SELF_EMPLOYED
+	
+}
---
+++ src/TaxDogg/TaxStatus.java
@@ -0,0 +1,15 @@
+package TaxDogg;
+
+/**
+ * List of accepted tax form statuses that user would be 
+ * able to select to find correct tax rates.
+ * 
+ * @author Last Place
+ */
+public enum TaxStatus {
+
+	SINGLE,
+	MARRIED,
+	HEAD_OF_HOUSEHOLD
+	
+}
