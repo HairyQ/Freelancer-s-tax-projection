04692b30af0ed53ea111947621372988a6ee92f8
Kris <kris.wolff@utah.edu>
2018-09-09 08:30:49 -0600

    Organized files
    Added in image files and all attempts of gui applications. Backend is functional, no one know front-end...yet ;)

---
+++ GUI/Unity/TaxDog/Assets/FirstToSecondPage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstToSecondPage : MonoBehaviour {
+
+	public void changeMenu(string sceneName) 
+	{
+		Application.LoadLevel(sceneName);
+	}
+}
---
+++ GUI/Xamarin/TaxDogg/TaxDogg/Fragments/Fragment1.cs
@@ -0,0 +1,31 @@
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+
+namespace TaxDogg.Fragments
+{
+    public class Fragment1 : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+            var entryName = new Entry { Text = "name entry" };
+            var name = entryName.Text;
+        }
+
+        public static Fragment1 NewInstance()
+        {
+            var frag1 = new Fragment1 { Arguments = new Bundle() };
+            return frag1;
+        }
+
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var ignored = base.OnCreateView(inflater, container, savedInstanceState);
+            return inflater.Inflate(Resource.Layout.fragment1, null);
+        }
+    }
+}
